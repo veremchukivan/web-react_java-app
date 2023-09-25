a7476b9c4ef17b80ffe1f378a252a2554e1633ca
veremchukivan <ivan2005verem@gmail.com>
2023-09-25 12:58:07 +0300

    admin panel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,15 @@
-{
-  "compilerOptions": {
-    "target": "ES2020",
-    "useDefineForClassFields": true,
-    "lib": ["ES2020", "DOM", "DOM.Iterable"],
-    "module": "ESNext",
-    "skipLibCheck": true,
-
-    /* Bundler mode */
-    "moduleResolution": "bundler",
-    "allowImportingTsExtensions": true,
-    "resolveJsonModule": true,
-    "isolatedModules": true,
-    "noEmit": true,
-    "jsx": "react-jsx",
-
-    /* Linting */
-    "strict": true,
-    "noUnusedLocals": true,
-    "noUnusedParameters": true,
-    "noFallthroughCasesInSwitch": true
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {},
   },
-  "include": ["src"],
-  "references": [{ "path": "./tsconfig.node.json" }]
+  plugins: [
+    // eslint-disable-next-line
+    require('flowbite/plugin')
+  ],
 }
